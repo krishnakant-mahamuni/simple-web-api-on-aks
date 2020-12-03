f03f3e7cbab06ad74702fb48067b834243f7437e
krishnakant <krishnakant.mahamuni@live.com>
2020-12-03 20:50:20 +1100

    missing variables

--- Infrastructure/azure/terraform/modules/cluster/variables.tf
+++ Infrastructure/azure/terraform/modules/cluster/variables.tf
@@ -12,3 +12,13 @@ variable "location" {
 variable "kubernetes_version" {
     default = "1.18.10"
 }
+variable "prefix" {
+  default = "simple-web-api"
+}
+variable "node_vm_size" {
+  default = "Standard_B2s"
+}
+variable "node_count" {
+  type    = number
+  default = 1
+}
