ff1cc86e71345becb714f7397f0f8c5b8395a214
krishnakant <krishnakant.mahamuni@live.com>
2020-12-03 20:47:36 +1100

    missing variables

--- Infrastructure/azure/terraform/main.tf
+++ Infrastructure/azure/terraform/main.tf
@@ -19,6 +19,9 @@ module "cluster" {
   resource_group_name     = var.resource_group_name
   location                = var.location
   kubernetes_version      = var.kubernetes_version
+  prefix                  = var.prefix
+  node_count              = var.node_count
+  node_vm_size            = var.node_vm_size
 }
 
 module "acr" {
